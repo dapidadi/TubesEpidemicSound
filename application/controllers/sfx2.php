cfe360f4ab8384437b6c1b2e7b442d83946cade7
aqmarinaalifah <47956938+aqmarinaalifah@users.noreply.github.com>
2019-05-06 11:54:40 +0700

    Create sfx2.php

---
+++ application/controllers/sfx2.php
@@ -0,0 +1,24 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class sfx2 extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('sfx_model');
+    }
+
+    public function index()
+    {
+        $data = array(
+
+            'title'     => 'Playlist',
+            'SFX'       => $this->sfx_model->tampil_data(),
+
+        );
+
+        $this->load->view('SFX', $data);
+    }
+}
