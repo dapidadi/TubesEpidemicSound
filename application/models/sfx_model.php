8401b2041ad508b9a0263d92926708e6ea18b58e
aqmarinaalifah <47956938+aqmarinaalifah@users.noreply.github.com>
2019-05-06 11:56:49 +0700

    Create sfx_model.php

---
+++ application/models/sfx_model.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class sfx_model extends CI_Model
+{
+    function tampil_data(){
+         $query = $this->db->select("*")
+                 ->from('dbnya')
+                 ->get();
+        return $query->result();
+    }
+}
+?>
