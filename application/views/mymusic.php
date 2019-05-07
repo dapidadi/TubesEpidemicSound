fa0a133208da71765e380c909ce79142c3f5a5c9
diraafwa <47936646+diraafwa@users.noreply.github.com>
2019-05-07 13:07:02 +0700

    Create mymusic.php

---
+++ application/views/mymusic.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Epidemic Sound</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  
+</head>
+<body>
+	<style>
+		.colornav
+	{
+  	background-color: rgb(241,241,241);
+	}
+	</style>
+
+	<!-- Navigation Bar -->
+	<nav class="navbar fixed-top colornav navbar-expand-lg navbar-light">
+  	<div class="container">
+    <a class="navbar-brand" href="#">
+          <img src="assets/img/logored.png">
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Browse
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Album</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">SFX</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Latest</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Account</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">My Account</a>
+            <a class="dropdown-item" href="#">My Music</a>
+            <a class="dropdown-item" href="#">Log Out</a>
+          </div>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- END OF Navigation Bar -->	 
+ <div class="container-fluid">
+  
+  <div class="row">
+    <div class="col-sm-3" style="background-color:yellow;">
+      <input type="text" style="border-radius: 0px; " placeholder="Search for playlists" name="search">
+      <input type="text" style="border-radius: 0px; border: 0px;" placeholder="+ New project" name="add">
+      <div class="dropdown">	
+      <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" style="border: 0px;">Private Project</button>
+  	  </div>
+    </div>
+    <div class="col-sm-9" style="background-color:pink;">
+        
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+    </div>
+  </div> 
+
+
+
+</body>
