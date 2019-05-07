dc7e33212db8336aff2c85e011910b1b1995ef72
diraafwa <47936646+diraafwa@users.noreply.github.com>
2019-05-07 12:50:29 +0700

    Create contact.php

---
+++ application/views/contact.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Epidemic</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css">
+
+</head> 
+<body>
+<style>
+
+.colornav
+{
+	background-color: rgb(241,241,241);
+}
+</style>
+
+
+<!-- Navigation Bar -->
+<nav class="navbar fixed-top colornav navbar-expand-lg navbar-light  ">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+          <img src="assets/img/logored.png">
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Browse</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Album</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">SFX</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Latest</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Account</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">My Account</a>
+            <a class="dropdown-item" href="#">My Music</a>
+            <a class="dropdown-item" href="#">Log Out</a>
+          </div>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- END OF Navigation Bar -->
+<div class = "container" style="text-align:left;">
+
+<div class="row"style="padding-top: 150px;">
+    <div class="col-sm-9" style="background-color:yellow;">
+      <h1><b>CONTACT</b></h1>
+      <p>
+      	<b>Help & Support</b><br>
+      	<a href="mailto:hello@epidemicsound.com">hello@epidemic.com</a>
+      </p>
+      <p>
+      <b>Submit Your Music</b><br>
+      <a title="Read more" href="">Read more</a>	
+	  </p>
+	  <p>	
+	  </p>
+	  <h2>NORDICS HQ</h2>
+	  <p>
+	  	<b>Sales</b><br>
+	  	<a href="Frederik@epidemicsound.com">Frederik@epidemicsound.com</a>
+	  </p>
+	  <p>
+	  	Sweden<br>
+        +46 (0)76-319 45 47<br>
+        <a href="frederik@epidemicsound.com">frederik@epidemicsound.com</a>
+	  </p>
+	  <p>
+	  	Norway<br>
+        +46 (0)70-091 58 25‬<br>
+        <a href="hedda@epidemicsound.com">hedda@epidemicsound.com</a>
+	  </p>
+	  <p>
+	  	Denmark<br>
+        +45 31 40 60 46<br>
+        <a href="mia@epidemicsound.com">mia@epidemicsound.com</a>
+	  </p>
+	  <p>
+	  	Finland<br>
+        +46 (0)72- 521 66 65<br>
+        <a href="oona@epidemicsound.com">oona@epidemicsound.com</a>
+	  </p>
+	  <p>
+	  	<b>In-Store Music</b><br>
+	  	+46 (0)76 000 20 44<br>
+	  	<a href="ism@epidemicsound.com">ism@epidemicsound.com</a>
+	  </p>
+	  <p>
+	  	<b>Address</b><br>
+	  	Epidemic Sound <br>
+	  	Åsögatan 121<br>
+		116 24 Stockholm<br>
+		SWEDEN<br>	
+	  </p>
+	  <h2> NORTH AMERICA</h2>
+	  <p>
+	  	<b>New York, Sales</b><br>
+	  	<a href="us@epidemicsound.com">us@epidemicsound.com</a>
+	  </p>
+	  <p>
+	  	<b>Address</b><br>
+	  	Epidemic Sound INC.<br>
+		79 Walker Street,<br>
+		New York, NY 10013<br>
+		USA
+	  </p>
+	  <p>
+	  	<b>Los Angeles, Sales</b><br>
+	  	<a href="us@epidemicsound.com">us@epidemicsound.com</a>
+	  </p>
+
+	  <h2>GERMANY (GSA)</h2>
+	  <p>
+	  	<b>Sales & In-Store Music</b><br>
+	  	+49 (40)94 77 90 50<br>
+	  	<a href="germany@epidemicsound.com">germany@epidemicsound.com</a>
+	  </p>
+	  <p>
+	  	Address<br>
+	  	Epidemic Sound DE GMBH<br>
+		Alte Pianofortefabrik<br>
+		Schulterblatt 58<br>
+		20357 Hamburg<br>
+		GERMANY<br>
+	  </p>
+	  <h2>BENELUX</h2>
+	  <p>
+	  	<b>Sales & In-Store Music</b><br>
+	  	+31 (0)6 1511 2734<br>
+	  	<a href="benelux@epidemicsound.com">benelux@epidemicsound.com</a>
+	  </p>
+	  <p>
+	  	<b>Address</b><br>
+	  	Epidemic Sound<br>
+		Passeerdersgracht 19A<br>
+		1016XG Amsterdam<br>
+		Netherlands<br>
+	  </p>
+	  <h2>UK</h2>
+	  <p>
+	 	<b>Sales & In-Store Music</b><br>
+	 	<a href="uk@epidemicsound.com">uk@epidemicsound.com</a>	
+	  </p>
+	  <h2>SPAIN & PORTUGAL</h2>
+	  <p>
+	  	<b>Sales & In-Store Music</b><br>
+	  	+34 (0)610 572 661<br>
+	  	<a href="spain@epidemicsound.com">spain@epidemicsound.com</a>
+	  </p>
+	  <h2>FRANCE</h2>
+	  <p>
+	  	<b>Sales & In-Store Music</b><br>
+	  	+33 (0)6 81 90 97 91<br>
+	  	<a href="france@epidemicsound.com">france@epidemicsound.com</a>
+	  </p>
+	  <h2>REST OF THE WORLD</h2><br>
+	  Strategic Partnerships & B2B Audiovisual/InStore Music Licensing
+	  <p>
+	  <b>Audiovisual</b><br>
+	  +46 (0)73 531 91 75<br>
+	  <a href="row@epidemicsound.com">row@epidemicsound.com</a>
+	  </p>
+	  <p>
+	  	<b>In-Store Music</b><br>
+	  	+46 (0)70 277 12 90<br>
+	  	<a href="row@epidemicsound.com">row@epidemicsound.com</a>
+	  </p>
+    </div>
+    
+  </div>
+</div>
+<!-- FOOTER -->
+
+<section style="background-color: black">
+  <br>
+  <div class="container-fluid" style="color: white; text-align: left;">
+    <div class="row">
+    <div class="col">
+    <ul style="list-style: none;">
+      <li><img src="assets/img/logo.png"></li>
+    </ul>
+    </div>
+    <div class="col">
+    <ul style="list-style: none;">
+      <li>Browse</li>
+      <li>Album</li>
+      <li>Latest Track</li>
+      <li>SFX</li>
+    </ul>
+    </div>
+    <div class="col">
+    <ul style="list-style: none;">
+      <li>Newsletter</li>
+      <li>Careers</li>
+      <li>Press</li>
+      <li>Contact</li>
+    </ul>
+    </div>
+    <div class="col">
+    <ul style="list-style: none;">
+      <li>In-Store Music</li>
+      <li>Pricing</li>
+      <li>Blog</li>
+      <li>Composers</li>
+      <li>Supports</li>
+    </ul>
+    </div>
+    </div>
+  </div>
+</section>
+
+<section style="background-color: black">
+  <br>
+  <div class="container-fluid" style="color: white;">
+    <div class="row">
+      <div class="col">
+        <ul style="list-style: none;">
+          logo
+          logo 
+          logo
+        </ul>
+      </div>
+      <div class="col">
+        <ul>
+          Copyright &copy Epidemic Sound 2019<br>
+          <a href="#">Privact Policy</a>
+          <a href="#">General Terms</a>
+          <a href="#">Cookie Policy</a>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- == JS FILE == -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
